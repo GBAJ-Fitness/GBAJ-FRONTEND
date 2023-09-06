7d49212e8fd4fc9bb00527b4f06102a801897389
Brian Kimbrow <bkimbrow25@gmail.com>
2023-09-06 10:45:11 -0500

    app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import axios from "axios";
+import Forms from "./Forms";
 
-function App() {
-  constructor (props) 
-  super(props)
-  this.state = {
-    item: [],
-    nutrition: false,
-    workouts: false,
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: [],
+      nutrition: false,
+      workouts: false,
+    };
+  }
 
+  // async componentDidMount() {
+  //   // Your async code here
+  // }
 
-  } 
-
-  return (
-    <>
-
-    </>
-  );
+  render() {
+    return (
+      <>
+        <div>We here </div>
+      </>
+    );
+  }
 }
 
 export default App;
