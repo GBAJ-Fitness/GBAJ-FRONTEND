5625f9aacbcbc71398a0f8fd12002d3f54a95816
Brian Kimbrow <bkimbrow25@gmail.com>
2023-09-07 00:27:49 -0500

    forms

--- src/Forms.jsx
+++ src/Forms.jsx
@@ -2,50 +2,7 @@
 
 
 
-// function BasicExample() {
-//   return (
-//     <CardGroup>
-//       {/* First Card */}
-//       <Card style={{ width: '18rem' }}>
-//         <Card.Img variant="top" src="holder.js/100px180?text=Image1" />
-//         <Card.Body>
-//           <Card.Title>Card Title 1</Card.Title>
-//           <Card.Text>
-//             Some quick example text to build on the card title and make up the
-//             bulk of the card's content.
-//           </Card.Text>
-//           <Button variant="primary">Go somewhere</Button>
-//         </Card.Body>
-//       </Card>
 
-//       {/* Second Card */}
-//       <Card style={{ width: '18rem' }}>
-//         <Card.Img variant="top" src="holder.js/100px180?text=Image2" />
-//         <Card.Body>
-//           <Card.Title>Card Title 2</Card.Title>
-//           <Card.Text>
-//             Some quick example text to build on the card title and make up the
-//             bulk of the card's content.
-//           </Card.Text>
-//           <Button variant="primary">Go somewhere</Button>
-//         </Card.Body>
-//       </Card>
-
-//       {/* Third Card */}
-//       <Card style={{ width: '18rem' }}>
-//         <Card.Img variant="top" src="holder.js/100px180?text=Image3" />
-//         <Card.Body>
-//           <Card.Title>Card Title 3</Card.Title>
-//           <Card.Text>
-//             Some quick example text to build on the card title and make up the
-//             bulk of the card's content.
-//           </Card.Text>
-//           <Button variant="primary">Go somewhere</Button>
-//         </Card.Body>
-//       </Card>
-//     </CardGroup>
-//   );
-// }
 
 // export default BasicExample;
 
