fd89e41e7a55a48886e2b8a9968d1b5c90a883ee
Brian Kimbrow <bkimbrow25@gmail.com>
2023-09-07 00:26:31 -0500

    forms

--- src/Forms.jsx
+++ src/Forms.jsx
@@ -17,20 +17,9 @@
 
 
 
-//       <Form.Group className="mb-3" controlId="formBasicPassword">
-//         <Form.Label>Password</Form.Label>
-//         <Form.Control type="password" placeholder="Password" />
-//       </Form.Group>
 
-//       <Form.Group className="mb-3" controlId="formBasicCheckbox">
-//         <Form.Check type="checkbox" label="Sign Me Up" />
-//       </Form.Group>
-//       <Button variant="primary" type="submit">
-//         Submit
-//       </Button>
-//     </Form>
-//   );
-// }
+
+
 
 // export default Forms;
 
