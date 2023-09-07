09296684ae311235aae6afa6528d92c9d7e0a619
Brian Kimbrow <bkimbrow25@gmail.com>
2023-09-07 00:27:11 -0500

    forms

--- src/Forms.jsx
+++ src/Forms.jsx
@@ -1,41 +1,5 @@
 
 
-
-
-
-
-
-
-
-
-
-        
-
-
-
-
-
-
-
-
-
-
-
-// export default Forms;
-
-
-
-//           Some quick example text to build on the card title and make up the
-//           bulk of the card's content.
-//         </Card.Text>
-//         <Button variant="primary">Go somewhere</Button>
-//       </Card.Body>
-//     </Card>
-//   );
-// }
-
-// export default BasicExample;
-
 // import Button from 'react-bootstrap/Button';
 // import Card from 'react-bootstrap/Card';
 // import CardGroup from 'react-bootstrap/CardGroup';
