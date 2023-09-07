02505d4fb906c73ba73a8092bfb2caaee1bb6815
Justin Agim <justinagim@gmail.com>
2023-09-07 12:55:14 -0500

    converted class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,52 @@
 import React, { useState } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import  ReactDOM  from "react";
-import Forms from './Forms';
+import "bootstrap/dist/css/bootstrap.min.css";
+import ReactDOM from "react";
+import Forms from "./Forms";
 import Auth from "./auth";
 import ModalComponent from "./Modal";
 import { Route, Routes } from "react-router-dom";
-import About from './About';
-import { NavLink,} from "react-router-dom"
-import "./App.css"
-
+import About from "./About";
+import { NavLink } from "react-router-dom";
+import "./App.css";
 
 // short curcuit
-function App () {
-  const [singingUp, setSigningUp] = useState(false)
-  return (
-    
-      <div style={backgroundStyle}>
-
-       <Auth/>
-        <nav>
-       <NavLink to="/about">About Us</NavLink>
-       <NavLink to="/">Home</NavLink>
-       </nav>
-        {singingUp && <ModalComponent setSigningUp={setSigningUp}/>}
-
-        <Routes>
-            <Route path="/" element={ <Forms setSigningUp={setSigningUp}/>} />
-            <Route path="/about" element={<About />} />
-
-          </Routes>
-        </div>
-    );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signingUp: false,
+    };
   }
 
+  setSigningUp = (s) => {
+    this.setState({
+      signingUp: s,
+    });
+  };
+  render() {
+    // const [singingUp, setSigningUp] = useState(false)
+    return (
+      <div style={backgroundStyle}>
+        <Auth />
+        <nav>
+          <NavLink to="/about">About Us</NavLink>
+          <NavLink to="/">Home</NavLink>
+        </nav>
+        {this.state.signingUp && (
+          <ModalComponent setSigningUp={this.setSigningUp} />
+        )}
+
+        <Routes>
+          <Route
+            path="/"
+            element={<Forms setSigningUp={this.setSigningUp} />}
+          />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    );
+  }
+}
 
 const backgroundStyle = {
   backgroundImage: `url("/public/Black & White Brick Grunge Fitness Gym Poster - 2.png")`,
@@ -41,6 +55,4 @@ const backgroundStyle = {
   minHeight: "100vh",
 };
 
-
 export default App;
-
