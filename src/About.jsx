2c5d570a00bfb8b3892901146ae5d29d32389fe0
Anya Fleming <anyafleming16@gmail.com>
2023-09-09 12:41:15 -0500

    finished About us page

--- src/About.jsx
+++ src/About.jsx
@@ -6,7 +6,11 @@ export default class About extends Component {
     
     return ( 
         <div>
-            <img src="/About Us.PNG" alt="picture" width="500" height="600"/>
+            <img src="/About Us.PNG" alt="picture" width="1400" height="900"/>
+            <img src="/Gerarad.png" alt="picture" width="400" height="500"/>
+            <img src="/Brian.png" alt="picture" width="400" height="500"/>
+            <img src="/Anya.png" alt="picture" width="400" height="500"/>
+            <img src="/Justin.png" alt="picture" width="400" height="500"/>
             <div>About Us</div> 
     
         </div>
