798153375093261df101d7590ffb44cb8d3b14a7
Anya Fleming <anyafleming16@gmail.com>
2023-09-11 20:44:31 -0500

    centered about us page content

--- src/About.jsx
+++ src/About.jsx
@@ -5,14 +5,29 @@ export default class About extends Component {
   render() {
     
     return ( 
-        <div>
-            <img src="/About Us.PNG" alt="picture" width="1200" height="1000"/>
+        <div
+        style={{
+         width: "100%",
+         display: 'flex',
+         flexDirection: 'column',
+         justifyContent: 'center',
+         alignItems: 'center',
+
+         
+         
+        }}
+
+        
+        >
+        
+            <img src="/About Us.PNG" alt="picture" width="1500" height="1200"/>
+            <div>
             <img src="/Gerarad.png" alt="picture" width="400" height="500"/>
             <img src="/Brian.png" alt="picture" width="400" height="500"/>
             <img src="/Anya.png" alt="picture" width="400" height="500"/>
             <img src="/Justin.png" alt="picture" width="400" height="500"/>
             <div>About Us</div> 
-    
+            </div>
         </div>
    
       
