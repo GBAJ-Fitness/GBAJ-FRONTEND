fd4becbf3f93c1005f28f02a4f6db60328c45b91
Brian Kimbrow <bkimbrow25@gmail.com>
2023-09-11 19:36:22 -0500

    fend delete public

--- src/Video.jsx
+++ src/Video.jsx
@@ -5,7 +5,7 @@ export default class Video extends Component {
   render() {
     return (
       <div style={{width:"100%", display:"flex", justifyContent:"center", marginBottom:"10rem", paddingTop:"5rem"}}>
-        <ReactPlayer url={/GBAJ.MP4'} controls={true} />
+        <ReactPlayer url={'/public/GBAJ.MP4'} controls={true} />
         </div>
     )
   }
